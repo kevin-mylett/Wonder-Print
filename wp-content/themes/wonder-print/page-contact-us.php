982fd9408dbeafd6bb22f8bf6d07df35890d0f43
kevin-mylett <kevinmylett@sky.com>
2017-04-12 12:03:39 +0100

    Created specific page template for contact-us page

--- wp-content/themes/wonder-print/page-contact-us.php
+++ wp-content/themes/wonder-print/page-contact-us.php
@@ -1,14 +1,15 @@
 <?php
-/**
- * The template used for displaying page content on business category pages
- * and the about-us page.
- *
- */
+/*
+* Page Template: Contact Us page
+*/
+
 $post_title = get_the_title();
 $title_as_array = explode(' ', $post_title);
 $last_word = array_pop($title_as_array);
 
 ?>
+
+<?php get_template_part('includes/headers/header', 'us' ); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header>
@@ -16,9 +17,7 @@ $last_word = array_pop($title_as_array);
 			<div class="heading-container <?php echo strtolower($last_word); ?>-bg">
 				<h1 class="category-title"><?php the_title(); ?></h1>
 			</div>
-			<div class="head-image">
-				<?php the_post_thumbnail(); ?>
-			</div>
+			<div class="head-image" id="map-container"></div>
 		</div>
 	</header><!-- .entry-header -->
 	
@@ -34,10 +33,10 @@ $last_word = array_pop($title_as_array);
 		</div><!-- .entry-content -->
 	</div><!-- .row -->
 
-	<?php get_template_part('includes/call-to-action'); ?>
-
 	<?php get_template_part('includes/clients'); ?>
 
 	<?php edit_post_link( __( 'Edit', '_tk' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-
+	
 </article><!-- #post-## -->
+
+<?php get_template_part('includes/footers/footer', 'business' ); ?>
