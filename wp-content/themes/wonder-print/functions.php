1238b67508bfe990f1f304001d99dd530176dc8a
kevin-mylett <kevinmylett@sky.com>
2017-05-12 14:45:08 +0100

    Added theme support for new product gallery

--- wp-content/themes/wonder-print/functions.php
+++ wp-content/themes/wonder-print/functions.php
@@ -116,6 +116,10 @@ remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
 add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
     add_theme_support( 'woocommerce' );
+    //Add product gallery support
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
 }
 
 /*
