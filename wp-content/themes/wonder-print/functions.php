9737c0f4d787c28cac1224f65dda47f3e44d04c1
kevin-mylett <kevinmylett@sky.com>
2017-05-19 15:41:40 +0100

    Replaced deprecated wc_add_to_cart_message filter

--- wp-content/themes/wonder-print/functions.php
+++ wp-content/themes/wonder-print/functions.php
@@ -235,15 +235,21 @@ function delivery_content() {
 }
 
 //Change button class of add-to-card notification
-add_filter ( 'wc_add_to_cart_message', 'wc_add_to_cart_message_filter', 10, 2 );
-function wc_add_to_cart_message_filter($message, $product_id = null) {
-    $titles[] = get_the_title( $product_id );
+add_filter( 'wc_add_to_cart_message_html', 'custom_cart_message_button_class', 10, 2 );
+function custom_cart_message_button_class($message, $products) {
+    $count  = 0;
 
-    $titles = array_filter( $titles );
-    $added_text = sprintf( _n( '%s has been added to your cart.', '%s have been added to your cart.', sizeof( $titles ), 'woocommerce' ), wc_format_list_of_items( $titles ) );
+    foreach ( $products as $product_id => $qty ) {
+        $titles[] = ( $qty > 1 ? absint( $qty ) . ' &times; ' : '' ) . sprintf( _x( '&ldquo;%s&rdquo;', 'Item name in quotes', 'woocommerce' ), strip_tags( get_the_title( $product_id ) ) );
+        $count += $qty;
+    }
 
+    $titles     = array_filter( $titles );
+    $added_text = sprintf( _n( '%s has been added to your cart.', '%s have been added to your cart.', $count, 'woocommerce' ), wc_format_list_of_items( $titles ) );
+
+    $titles     = array_filter( $titles );
+    $added_text = sprintf( _n( '%s has been added to your cart.', '%s have been added to your cart.', $count, 'woocommerce' ), wc_format_list_of_items( $titles ) );
     $message = sprintf( '%s<a href="%s" class="btn-woo-notice">%s</a>', esc_html( $added_text ), esc_url( wc_get_page_permalink( 'cart' ) ), esc_html__( 'View Cart', 'woocommerce' ) );
-
     return $message;
 }
 
